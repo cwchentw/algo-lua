ded10f44b4326de1791022ae70a530c03556b7c0
Michael Chen <cwchen123@gmail.com>
2017-01-26 20:26:41 +0800

    Update lib path

--- lib/algo/DoubleVector.lua
+++ lib/algo/DoubleVector.lua
@@ -25,16 +25,25 @@ ffi.cdef([[
 local cvector = nil
 local os_type, _ = util.get_os()
 
-local home = os.getenv("HOME")
-local rockpath_unix = "/.luarocks/lib/lua/5.1/"
-local rockpath_win = "\\.luarocks\\lib\\lua\\5.1\\"
+-- Refactor it later.
+local version = _VERSION:match("%d+%.%d+")
+local prefix = os.getenv("PREFIX") or os.getenv("HOME")
+local rockpath_unix = nil
+local rockpath_win = nil
+if prefix == os.getenv("HOME") then
+  rockpath_unix = "/.luarocks/lib/lua/" .. version .. "/"
+  rockpath_win = "\\.luarocks\\lib\\lua\\" .. version .. "\\"
+else
+  rockpath_unix = "/lib/luarocks/lib/lua/" .. version .. "/"
+  rockpath_win = "\\lib\\luarocks\\lib\\lua\\" .. version .. "\\"
+end
 
 if os_type == "Windows" then
-  cvector = ffi.load(home .. rockpath_win .. "libdoubleVector.dll")
+  cvector = ffi.load(prefix .. rockpath_win .. "libdoubleVector.dll")
 elseif os_type == "Mac" then
-  cvector = ffi.load(home .. rockpath_unix .. "libdoubleVector.dylib")
+  cvector = ffi.load(prefix .. rockpath_unix .. "libdoubleVector.dylib")
 elseif os_type == "Linux" then
-  cvector = ffi.load(home .. rockpath_unix .. "libdoubleVector.so")
+  cvector = ffi.load(prefix .. rockpath_unix .. "libdoubleVector.so")
 else
   error("Unsupported platform")
 end
